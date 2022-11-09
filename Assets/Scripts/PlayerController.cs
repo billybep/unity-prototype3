e13c9ea367acf726c7108ca8047361dc13d8960f
billybep <billy.pesoth@gmail.com>
2022-11-10 02:38:19 +0800

    Player jump, prevent double jump, add obstacle, spawn with intervals

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,16 +6,30 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rbPlayer;
 
+    public float jumpForce = 5;
+    public float gravityModifier;
+    public bool isOnGround = true;
+
     // Start is called before the first frame update
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
-        rbPlayer.AddForce(Vector3.up * 450);
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            // ForceMode.Impulse parameter to the AddForce call, then reduce force multiplier value
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
