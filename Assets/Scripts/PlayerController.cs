10bef97373bab30bfc30653c051cb8559989d6a3
billybep <billy.pesoth@gmail.com>
2022-11-10 00:33:26 +0800

    test jump at start the game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Rigidbody rbPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbPlayer = GetComponent<Rigidbody>();
+        rbPlayer.AddForce(Vector3.up * 450);
     }
 
     // Update is called once per frame
